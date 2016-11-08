c7204ce12432ec7dc8e35587e9d681404ec84f24
Jose De Ita <jcruzdei@pslcorp.com>
2016-11-07 18:59:37 -0600

    Replace jasmine with cucumber framework

--- conf.js
+++ conf.js
@@ -2,10 +2,13 @@ var env = require('./environment.js');
 
 exports.config = {
     seleniumServerJar: env.seleniumServerJar,
+    getPageTimeout: 60000,
+    allScriptsTimeout: 500000,
     specs: env.specs,
     baseUrl: env.baseUrl,
     framework: env.framework,
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
     capabilities: env.capabilities,
-    jasmineNodeOpts: env.jasmineNodeOpts,
+    cucumberOpts: env.cucumberOpts,
     onPrepare: env.onPrepare
 };
--- environment.js
+++ environment.js
@@ -4,8 +4,8 @@ module.exports = {
     // Path of the selenium server
     seleniumServerJar: "node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar",
     // Spec patterns
-    specs: ['e2e/**/*test.js'],
-    framework: 'jasmine2',
+    specs: ['features/*.feature'],
+    framework: 'custom',
     baseUrl: baseUrl,
 
     // Capabilities to be passed to the webdriver instance.
@@ -13,15 +13,12 @@ module.exports = {
         browserName: 'chrome'
     },
 
-    // Jasmine node options
-    // See https://github.com/jasmine/jasmine-npm/blob/master/lib/jasmine.js
-    jasmineNodeOpts: {
-        showColors: true,
-        displayStacktrace: true,
-        displaySpecDuration: true,
-        // overrides jasmine's print method to report dot syntax for custom reports
-        print: function () {},
-        defaultTimeoutInterval: 50000
+    cucumberOpts: {
+        require: 'features/step_definitions/*.js',
+        tags: false,
+        format: 'pretty',
+        profile: false,
+        'no-source': true
     },
 
     onPrepare: function () {
@@ -29,15 +26,6 @@ module.exports = {
         browser.manage().timeouts().implicitlyWait(5000);
         // set browser size...
         browser.manage().window().setSize(1024, 800);
-
-
-        var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
-        var SpecReporter = require('jasmine-spec-reporter');
-
-        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
-            savePath: './reports/'
-        }));
-        jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'spec'}));
 
         browser.ignoreSynchronization = true;
         browser.driver.get(baseUrl + '/en-us/?app=office-dev');
